a30b1dabf6c8b06ea78ad07c99e05e736e975367
NancyN00 <nancynyabuti10@gmail.com>
2023-11-06 04:39:56 +0300

    first commit

---
+++ app/src/main/java/com/example/weatherapp/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.example.weatherapp.di
+
+import com.example.weatherapp.api.ApiService
+import com.example.weatherapp.utils.Constants
+import com.example.weatherapp.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideBaseUrl() = Constants.BASE_URL
+
+    @Provides
+    @Singleton
+    fun providesRetrofitInstance(): ApiService =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+
+}
